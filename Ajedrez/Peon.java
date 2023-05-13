d5f83c1019f344a7ed3c0492b9517c4c197de6d8
FoxyYTs <foxy200442@gmail.com>
2023-05-12 21:02:45 -0500

    a

--- Ajedrez/Peon.java
+++ Ajedrez/Peon.java
@@ -1,26 +1,8 @@
 package Ajedrez;
 
 public class Peon extends Fichas{
-    private String posición,color;
     public Peon(String posicion, String color){
         super(posicion, color);
-        this.posición = posicion;
-        this.color = color;
     }
 
-    public void setPosicion(String posicion){
-        this.posición = posicion;
-    }
-
-    public String getPosicion(){
-        return posición;
-    }
-
-    public void setColor(String color){
-        this.color = color;
-    }
-
-    public String getColor(){
-        return color;
-    }
 }
